f168f15640a412c85a2addc2b9de8fff3c41fcfb
sidik <kasidik@siberat.id>
2024-04-16 08:50:51 +0700

    Load Image from assets

---
+++ lib/pages/ember_quest_game.dart
@@ -0,0 +1,18 @@
+import 'package:flame/game.dart';
+
+class EmberQuestGame extends FlameGame{
+  EmberQuestGame();
+
+  Future<void> onLoad() async {
+    // Load your game assets here
+    await images.loadAll([
+      'block.png',
+      'ember.png',
+      'ground.png',
+      'heart.png',
+      'heart_half.png',
+      'star.png',
+      'water_enemy.png'
+    ]);
+  }
+}
